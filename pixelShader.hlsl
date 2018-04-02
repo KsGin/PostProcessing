debc6bcbea9467306a1c0031d6d23e970e1ab9cd
KsGin <imqqyangfan@gmail.com>
2018-04-02 22:45:04 +0800

    Update

--- pixelShader.hlsl
+++ pixelShader.hlsl
@@ -7,7 +7,7 @@ struct pixelInputType {
 };
 
 float4 Inversion(float4 color) {
-	color = float4(1.0f , 1.0f , 1.0f , 1.0f) - color;
+	color = float4(1.0f, 1.0f, 1.0f, 1.0f) - color;
 	color.w = 1.0f;
 	return color;
 }
@@ -18,7 +18,7 @@ float4 Grayscale(float4 color) {
 }
 
 
-float4 doFilter(float3x3 fil , float2 texSize , float2 uv) {
+float4 doFilter(float3x3 fil, float2 texSize, float2 uv) {
 	float2 posDelta[3][3] = {		// 3x3偏移坐标
 		{float2(-1.0f , -1.0f) , float2(0.0f , -1.0f) , float2(1.0f , -1.0f)} ,
 		{float2(-1.0f , 0.0f) , float2(0.0f , 0.0f) , float2(1.0f , 0.0f)},
@@ -29,7 +29,7 @@ float4 doFilter(float3x3 fil , float2 texSize , float2 uv) {
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			float2 tUV = float2(uv.x + posDelta[i][j].x , uv.y + posDelta[i][j].y);
+			float2 tUV = float2(uv.x + posDelta[i][j].x, uv.y + posDelta[i][j].y);
 			// 加到最终颜色上
 			color += tex.Sample(samp, tUV / texSize) * fil[i][j];
 		};
@@ -41,10 +41,10 @@ float4 doFilter(float3x3 fil , float2 texSize , float2 uv) {
 float4 main(pixelInputType input) : SV_TARGET{
 	float2 texSize = float2(1920 , 1200);		// 图片的大小
 	float3x3 fil = float3x3(
-		1.0f, 2.0f, 1.0f, 
-		2.0f, 4.0f, 2.0f, 
-		1.0f, 2.0f, 1.0f
-	) / 16;
+		-1.0f, -2.0f, -1.0f,
+		-2.0f, 13.0f, -2.0f,
+		-1.0f, -2.0f, -1.0f
+	);
 	float2 uv = input.texcoord * texSize;
-	return doFilter(fil , texSize , uv);	
-} 
+	return doFilter(fil , texSize , uv);
+}
